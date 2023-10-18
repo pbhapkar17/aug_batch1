1616dfede993cb400621515a955d4a481bc5f7b4
pbhapkar17 <poonambhapkar34@gmail.com>
2023-10-18 17:52:40 +0530

    logical operators

--- consoleTestCase.js
+++ consoleTestCase.js
@@ -15,3 +15,21 @@ console.log(b + c); //2030
 console.log(a + b + c);//3030
 console.log(c + b + a); //302010
 
+console.log( b + a + c + a + b);//30301020
+
+let city ='mumbai';
+let d = 40;
+
+console.log(d+city);
+console.log(city+d);
+console.log(d+city+b);
+console.log(d+city+b+a);
+console.log(a+d+city+b+a);
+
+console.log("a",a);
+console.log("a" + a);
+console.log(a,b,c,d,city);
+console.log('a',a,'b',b,'c',c,'d',d,'city',city);
+
+
+
