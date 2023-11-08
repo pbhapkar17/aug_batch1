594d44fa7c8eba9dc316eea4998af9bd1c4e8ab7
aashishpa <aashishpatil598@gmail.com>
2023-11-08 12:38:18 +0530

    va

---
+++ practice.js
@@ -0,0 +1,37 @@
+
+// for loop
+// for of loop
+// for in loop
+// do while loop
+
+// for loop
+for(let a = 1 ; a <= 1000 ; a++){
+            console.log(a);}
+
+// for in loop
+            let student = {
+                name:'pooja', 
+                age : 20,
+                city:'pune'}
+
+            for(let x in student){
+      console.log(student[x]);   //[]property access symbol [x]
+            }
+
+            //for of loop
+let color = ['red','orange','blue','purple','pink'];
+for(let item of color){
+   console.log(item);
+}
+
+//do while loop
+
+let a=0;
+do{
+    console.log(a);
+    a++
+}
+while(a<=5){
+
+}
+
