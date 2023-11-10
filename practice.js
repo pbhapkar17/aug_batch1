801cc0261665b1b43bac33e4ce016cd34f18ffb4
aashishpa <aashishpatil598@gmail.com>
2023-11-10 19:37:28 +0530

    js

--- practice.js
+++ practice.js
@@ -1,37 +1,48 @@
 
-// for loop
-// for of loop
-// for in loop
-// do while loop
+// // for loop
+// // for of loop
+// // for in loop
+// // do while loop
 
-// for loop
-for(let a = 1 ; a <= 1000 ; a++){
-            console.log(a);}
+// // for loop
+// for(let a = 1 ; a <= 10; a++){
+//             console.log(a);}
 
-// for in loop
-            let student = {
-                name:'pooja', 
-                age : 20,
-                city:'pune'}
+// // for in loop
+//             let student = {
+//                 name:'pooja', 
+//                 age : 20,
+//                 city:'pune'}
 
-            for(let x in student){
-      console.log(student[x]);   //[]property access symbol [x]
-            }
+//             for(let x in student){
+//       console.log(student[x]);   //[]property access symbol [x]
+//             }
 
-            //for of loop
-let color = ['red','orange','blue','purple','pink'];
-for(let item of color){
-   console.log(item);
+//             //for of loop
+// let color = ['red','orange','blue','purple','pink'];
+// for(let item of color){
+//    console.log(item);
+// }
+
+// //do while loop
+
+// let a=0;
+// do{
+//     console.log(a);
+//     a++
+// }
+// while(a<=5){
+
+// }
+ 
+
+let person=
+{
+
+firstName:"Aashish",
+lastName:"patil",
+age:"24",
+  eyeColour:"Blue" 
+    
 }
-
-//do while loop
-
-let a=0;
-do{
-    console.log(a);
-    a++
-}
-while(a<=5){
-
-}
-
+document.getElementById ("demo").innerHTML=person.firstName +""+person.lastName
