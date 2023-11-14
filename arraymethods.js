2cc8ac0bc3155f934c12ca4687f294729d2d44c0
aashishpa <aashishpatil598@gmail.com>
2023-11-14 19:31:40 +0530

    c

--- arraymethods.js
+++ arraymethods.js
@@ -1,68 +1,68 @@
-let city = ['pune','nashik','mumbai','jaipur',2000]; //hetrogenous array
-console.log(city.length); //length opr will return length of an array
+// // // // let city = ['pune','nashik','mumbai','jaipur',2000]; //hetrogenous array
+// // // // console.log(city.length); //length opr will return length of an array
 
-//toString()
-console.log(city.toString());
+// // // // //toString()
+// // // // console.log(city.toString());
 
-//pop() // pops out/delete last item from array and will return the same
-console.log(city);
-let popedOutItem = city.pop();
-console.log(popedOutItem);
-console.log(city);
-
-
-//push() to append/add items at the end of the array,will return length of the original array
-let returnedByPush = city.push(6000,'poonam');
-console.log(city);
-console.log(returnedByPush);
-
-//The shift() method removes the first array element and "shifts" all other elements to a lower index.
-let color  = ['red','blue','black','yellow'];
-let shiftMethodReturenedData = color.shift();
-console.log(shiftMethodReturenedData);
-console.log(color);
-
-//unshift()
-let color2  = ['pink','red','blue','black','yellow'];
-let returnedData = color2.unshift('black','orange');
-console.log(returnedData);
-console.log(color2);
-
-//join()
-let v = ['pink','red','blue','black','yellow'];
-let v1 = v.join(' ');
-console.log(v1);
-
-// let name = ["poonam", "patil"]
-let name = ["04", "05" ,"1969"]
-console.log(name);
-let fName = name.join("/");
-console.log(fName);
+// // // // //pop() // pops out/delete last item from array and will return the same
+// // // // console.log(city);
+// // // // let popedOutItem = city.pop();
+// // // // console.log(popedOutItem);
+// // // // console.log(city);
 
 
-//concat()
-cl2 = ['black','yellow'];
-cl3  = [];
-let cl4 = cl2.concat(cl3,v,"white") ;
-console.log(cl4);
+// // // // //push() to append/add items at the end of the array,will return length of the original array
+// // // // let returnedByPush = city.push(6000,'poonam');
+// // // // console.log(city);
+// // // // console.log(returnedByPush);
 
-//delete: undefined holes
- //length remains same after deleting item
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-delete fruits[0];
-//delete fruits[10];
-console.log('>>>',fruits);
+// // // // //The shift() method removes the first array element and "shifts" all other elements to a lower index.
+// // // // let color  = ['red','blue','black','yellow'];
+// // // // let shiftMethodReturenedData = color.shift();
+// // // // console.log(shiftMethodReturenedData);
+// // // // console.log(color);
 
-//splice(index(where u want to add item), count(how many elements want to delete, elements to be added))
-const fruit1 = ["Banana", "Orange", "Apple", "Mango"];
- //fruit1.splice(1,0,'kiwi');
-fruit1.splice(1,2,'kiwi');
-console.log(fruit1);
+// // // // //unshift()
+// // // // let color2  = ['pink','red','blue','black','yellow'];
+// // // // let returnedData = color2.unshift('black','orange');
+// // // // console.log(returnedData);
+// // // // console.log(color2);
 
-const fruit2 = ["Banana", "Orange", "Apple", "Mango",'kiwi'];
-let a = fruit2.slice(2,3);
-console.log(a);
-console.log(fruit2);
+// // // //join()
+// // // let v = ['pink','red','blue','black','yellow'];
+// // // let v1 = v.join(' ');
+// // // console.log(v1);
+
+// // // let name = ["poonam", "patil"]
+// // // let birth = ["04", "05" ,"1969"]
+// // // console.log(name);
+// // // let fName = name.join("/");
+// // // console.log(fName);
+
+
+// // //concat()
+// // cl2 = ['black','yellow'];
+// // cl3  = [];
+// // let cl4 = cl2.concat("white") ;
+// // console.log(cl4);
+
+// //delete: undefined holes
+//  //length remains same after deleting item
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0];
+// //delete fruits[10];
+// console.log('>>>',fruits);
+
+// //splice(index(where u want to add item), count(how many elements want to delete, elements to be added))
+// const fruit1 = ["Banana", "Orange", "Apple", "Mango"];
+//  //fruit1.splice(1,0,'kiwi');
+// fruit1.splice(1,2,'kiwi');
+// console.log(fruit1);
+
+// // const fruit2 = ["Banana", "Orange", "Apple", "Mango",'kiwi'];
+// // let a = fruit2.slice(2,3);
+// // console.log(a);
+// // console.log(fruit2);
 
 
 //number mrthods
--- practice.js
+++ practice.js
@@ -1,48 +1,106 @@
 
-// // for loop
-// // for of loop
-// // for in loop
-// // do while loop
+// // // // for loop
+// // // // for of loop
+// // // // for in loop
+// // // // do while loop
 
-// // for loop
-// for(let a = 1 ; a <= 10; a++){
-//             console.log(a);}
+// // // // for loop
+// // // for(let a = 1 ; a <= 10; a++){
+// // //             console.log(a);}
 
-// // for in loop
-//             let student = {
-//                 name:'pooja', 
-//                 age : 20,
-//                 city:'pune'}
+// // // // for in loop
+// // //             let student = {
+// // //                 name:'pooja', 
+// // //                 age : 20,
+// // //                 city:'pune'}
 
-//             for(let x in student){
-//       console.log(student[x]);   //[]property access symbol [x]
-//             }
+// // //             for(let x in student){
+// // //       console.log(student[x]);   //[]property access symbol [x]
+// // //             }
 
-//             //for of loop
-// let color = ['red','orange','blue','purple','pink'];
-// for(let item of color){
-//    console.log(item);
-// }
+// // //             //for of loop
+// // // let color = ['red','orange','blue','purple','pink'];
+// // // for(let item of color){
+// // //    console.log(item);
+// // // }
 
-// //do while loop
+// // // //do while loop
 
-// let a=0;
-// do{
-//     console.log(a);
-//     a++
-// }
-// while(a<=5){
+// // // let a=0;
+// // // do{
+// // //     console.log(a);
+// // //     a++
+// // // }
+// // // while(a<=5){
 
-// }
+// // // }
  
 
-let person=
-{
+// // // let person=
+// // // {
 
-firstName:"Aashish",
-lastName:"patil",
-age:"24",
-  eyeColour:"Blue" 
+// // // firstName:"Aashish",
+// // // lastName:"patil",
+// // // age:"24",
+// // //   eyeColour:"Blue" 
     
-}
-document.getElementById ("demo").innerHTML=person.firstName +""+person.lastName
+// // // }
+// // // document.getElementById ("demo").innerHTML=person.firstName +""+person.lastName
+
+
+// //   function func(2,4);
+// //   document.getElementById("demo").innerHTML=x;
+
+// //   function Myfunction(a,b){
+
+// //     return a*b;
+
+// //   }
+ 
+
+// let color =['pink','blue','yellow','red','green'];
+// console.log(color.length);
+
+// // var colorItems = color.pop();
+// // console.log(color);
+
+// let Coloradded =color.push('black','white','voilet');
+// console.log(color);
+
+// let shiftMethodReturenedData= color.shift();
+// console.log(color);
+
+// let unshift=color.unshift("purple");
+// console.log(color);
+
+let name =["pravin","pavan","onkar","sahil","ganesh"]
+let name1=name.join("  ");
+console.log(name);
+console.log(name1);
+
+let fName=["Aashish","patil"];
+
+let joinName=fName.join("  ");
+console.log(joinName);
+
+let DOb=['11','10','2001']
+let joinDOb = DOb.join("/");
+console.log(joinDOb);
+
+//Concat Method
+
+let c1=['black','yellow','blue'    ];
+
+let cl3=c1.concat('pink','orange','red');
+console.log(cl3);
+
+//DeleteMethod()
+
+let D1=["Aashish","onkar","rohit","priya"];
+delete D1[1];
+console.log(D1);
+
+//spliceMethod()
+
+let d1=D1.splice(1,3,"arnav");
+console.log(D1);
