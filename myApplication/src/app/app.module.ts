0783a434b47487a6d76377ca3075e4b9c8c54ccb
pbhapkar17 <poonambhapkar34@gmail.com>
2023-11-23 17:54:49 +0530

    simple routing

--- myApplication/src/app/app.module.ts
+++ myApplication/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
+    SignUpComponent,
     
   ],
   imports: [  //modules
